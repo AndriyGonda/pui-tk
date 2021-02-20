f65f685f33d40dd39e6a8886fddff9449f2255ca
Andriy <andriy.gonda@gmail.com>
2021-02-20 15:56:40 +0200

    setup library build

---
+++ README.md
@@ -0,0 +1,18 @@
+# PUI-Tk library
+
+## Description
+Simplified interface for Tkinter UI library based on python >3.6 features.
+
+### Features
+ - Improved ttkthemes loader. The default theme is Breeze (KDE).
+ - Matplotlib figures support.
+ -  __Custom Tkinter Layouts__:
+    - Grid Layout
+    - Row Layout
+    - Column Layout
+ - __Custom Components__:
+    - Chart component (support Matplotlib figures and canvas)
+    - LabeledEntry (Combines Tkinter Label and Entry)
+    - Custom Checkbox (possibilities to direct get checkbox state)
+    - Custom Radiobutton (possibilities to direct get radiobutton state)
+    - Custom event callback binding
--- setup.py
+++ setup.py
@@ -1,11 +1,34 @@
 from setuptools import find_packages, setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='pui_tk',
-    packages=find_packages(include=['pui_tk']),
-    version='0.0.2',
+    version_config={
+            "template": "{tag}",
+            "dev_template": "{tag}.dev{ccount}+git.{sha}",
+            "dirty_template": "{tag}.dev{ccount}+git.{sha}.dirty",
+            "starting_version": "0.0.1",
+            "version_callback": None,
+            "version_file": None,
+            "count_commits_from_version_file": False
+        },
+    setup_requires=['setuptools-git-versioning'],
+    packages=find_packages(include=['pui_tk', 'pui_tk.widgets']),
     description='Python UI library based on Tkinter',
-    author='A.Honda <andriy.gonda@gmail.com>',
+    author='A.Honda',
+    author_email='andriy.gonda@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/AndriyGonda/pui-tk',
     license='MIT',
-    install_requires=[],
+    install_requires=required,
+    package_data={
+        'pui_tk': ['icons/*']
+    },
+    platforms='any',
+    python_requires='>=3.6'
 )
