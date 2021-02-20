b219fe901eb23ca4e556400832d241defd0b13fd
Andriy <andriy.gonda@gmail.com>
2021-02-20 14:26:04 +0200

    refactored row layout

--- pui_tk/widgets/layouts/row.py
+++ pui_tk/widgets/layouts/row.py
@@ -3,23 +3,13 @@ from pui_tk.widgets import Frame
 
 
 class Row(Frame):
-    def __init__(self, base_widget):
-        super().__init__(base_widget)
-        self._columns = 0
+    def __init__(self, base_widget, **kwargs):
+        super().__init__(base_widget, **kwargs)
 
-    def append(self, widget: UiWidget,
-               margin_x: int or tuple = (0, 5),
-               margin_y: int or tuple = (0, 5),
-               padding_x: int = 0,
-               padding_y: int = 0,
-               sticky='nsew'):
-        widget.tk_ref.grid(
-                row=0,
-                column=self._columns,
-                padx=margin_x,
-                pady=margin_y,
-                ipadx=padding_x,
-                ipady=padding_y,
-                sticky=sticky
-            )
-        self._columns += 1
+    @staticmethod
+    def append(widget: UiWidget, expand=False, fill=False):
+        widget.tk_ref.pack(side='left', fill='both' if fill else 'none', expand=expand)
+
+    @staticmethod
+    def append_reverse(widget: UiWidget, expand=False, fill=False):
+        widget.tk_ref.pack(side='right', fill='both' if fill else 'none', expand=expand)
