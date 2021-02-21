7c0504fe433949aca177ad11bd4daaa00ab57db4
Andriy <andriy.gonda@gmail.com>
2021-02-21 13:34:10 +0200

    added frame props

--- pui_tk/widgets/frame.py
+++ pui_tk/widgets/frame.py
@@ -1,6 +1,15 @@
-from tkinter import Frame as _Frame
+from tkinter.ttk import Frame as _Frame
 from .ui_widget import UiWidget
+from pui_tk.types import TkProperty, Cursor, Relief
 
 
 class Frame(UiWidget):
     widget_class = _Frame
+    style = TkProperty('style', str)
+    takefocus = TkProperty('takefocus', bool)
+    width = TkProperty('width', (int, str))
+    height = TkProperty('height', (int, str))
+    padding = TkProperty('padding', (int, str))
+    cursor = TkProperty('cursor', (str, Cursor))
+    relief = TkProperty('relief', (str, Relief))
+
